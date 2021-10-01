57db862feef79272dae6365acc94a04350fc58b0
Morshed-Ahmed <morshed50r@gmail.com>
2021-10-01 16:17:50 +0600

    update the invantory page

---
+++ src/components/Invatory/Invatory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Invatory = () => {
+    return (
+        <div>
+            <h3>This is invantory page</h3>
+        </div>
+    );
+};
+
+export default Invatory;
